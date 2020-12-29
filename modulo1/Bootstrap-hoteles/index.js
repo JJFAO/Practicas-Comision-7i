cbdc5805b10a8d92f59422396c040066159c86e7
JJFAO <jjfalonso@gmail.com>
2020-12-28 21:46:43 -0300

    Introduccion Javascript: funciones y variables.

---
+++ modulo1/Bootstrap-hoteles/index.js
@@ -0,0 +1,10 @@
+// Muestro una alerta.
+// alert('Hola Mundo');
+// console.log(confirm('Elija si o no'));
+// console.log('Esto es un log')
+
+let nombre = 'Otro nombre'
+let edad = 54
+let acepta = confirm('Elija si o no');
+console.log(nombre, edad)
+console.log('El usuario presionó: ', acepta)
