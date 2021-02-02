58f5b555669245a1dd54d7c61b1fd22e3b6f95b0
JJFAO <jjfalonso@gmail.com>
2021-02-02 01:50:45 -0300

    Javascript: introduccion parte 3 - bucles, condicionales y operadores.

---
+++ modulo2/Intro-javascript-parte2/evaluarparidad.js
@@ -0,0 +1,28 @@
+// Se necesita un programa que pueda evaluar si un número ingresado es par.
+// - Pedir un número al usuario.
+// - Evaluar si es par el número ingresado.
+// - Mostrar el resultado indicando si es par o impar.
+
+function evaluarParidad() {
+    // Esta función llama a prompt() para tomar un número ingresado por el usuario.
+    const numero = prompt('Ingrese un número');
+    const noValido = validarNumero(numero);
+    // Luego se evalúa si el valor ingresado no es un número, si no lo es se muestra una alerta.
+    if (noValido) {
+        alert(`El valor ingresado: ${numero}. No es un número valido.`);
+        return;
+    }
+
+    const resto = parseInt(numero) % 2;
+    const esPar = resto === 0;
+    if (esPar) {
+        alert(`El valor ingresado: ${numero}. Es un número par.`);
+    } else {
+        alert(`El valor ingresado: ${numero}. Es un número impar.`);
+    }
+}
+
+function validarNumero(numero) {
+    // Esta función devuelve verdadero si alguno de las 3 operaciones resulta en verdadero.
+    return numero === null || numero === '' || isNaN(numero);
+}
---
+++ modulo2/Intro-javascript-parte2/index.js
@@ -0,0 +1,29 @@
+// Un uso que le daremos a las funciones es el de contener una secuencia de instrucciones
+// que queramos ejecutar y posiblemente reutilizar. ('modularizar')
+// Sería conveniente separar los ejercicios de práctica en distintas funciones por este motivo,
+// y para evitar tener mucho código comentado.
+
+// Para este ejemplo vamos a llamar a estas funciones en el evento click de un botón en el html.
+
+// Declaramos una función bucleEvaluarParidad que llamara a su vez a evaluarParidad.
+function bucleEvaluarParidad3() {
+    // Esta función tiene un bucle que se repetirá mientras que se cumpla la condición indicada.
+    // Cuando esta no se cumpla, es decir que la condición resulte en false, saldrá del bucle.
+    let contador = 0;
+    while (contador < 3) {
+        // Mientras el contador sea menor a 3, el resultado será true.
+        evaluarParidad();
+        contador++;
+        // el operador ++ le suma +1 a contador, esto es lo que hará que el bucle deje de repetirse cuando no se cumpla la condición.
+    }
+    console.log('Terminó el bucle');
+}
+
+function evaluarParidadEnBucle() {
+    // Aquí el bucle se repetirá mientras el usuario presione Aceptar en la ventana del confirm().
+    let aceptar = true;
+    while (aceptar) {
+        evaluarParidad();
+        aceptar = confirm('¿Desea ingresar otro número para evaluar?');
+    }
+}
