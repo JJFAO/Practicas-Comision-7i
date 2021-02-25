4ef46565214d4132ca2ed035eefb52d2c8b9ec54
JJFAO <jjfalonso@gmail.com>
2021-02-24 21:49:54 -0300

    LocalStorage: código con comentarios.

--- modulo2/LocalStorage-crud/index.js
+++ modulo2/LocalStorage-crud/index.js
@@ -3,8 +3,8 @@ const emailInput = document.getElementById('inputEmail');
 const passInput = document.getElementById('inputPass');
 const nombreInput = document.getElementById('inputNombre');
 const rolInput = document.getElementById('inputRol');
-const json = localStorage.getItem('usuarios');
-const data = JSON.parse(json);
+const json = localStorage.getItem('usuarios'); // Traer de localStorage el dato asociado a la key usuarios.
+const data = JSON.parse(json); // Convertir datos de un string JSON a código JavaScript.
 const usuarios = data || [];
 
 formularioForm.onsubmit = function (e) {
@@ -16,7 +16,7 @@ formularioForm.onsubmit = function (e) {
         rol: rolInput.value,
     };
     usuarios.push(usuario);
-    const json = JSON.stringify(usuarios);
-    localStorage.setItem('usuarios', json);
+    const json = JSON.stringify(usuarios); // Convertir datos a un string JSON.
+    localStorage.setItem('usuarios', json); // Guardar en localStorage un dato asociado a la key "usuarios".
 };
 
