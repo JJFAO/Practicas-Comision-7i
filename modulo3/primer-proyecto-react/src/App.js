0e7692c6bb059973ad04b47a3d16c894cf8ba027
JJFAO <jjfalonso@gmail.com>
2021-03-31 22:10:22 -0300

    React: práctica de componentes y props.

--- modulo3/primer-proyecto-react/src/App.js
+++ modulo3/primer-proyecto-react/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from './components/Nav';
+import Card from './components/Card';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const image1 = "https://images.unsplash.com/photo-1593642634402-b0eb5e2eebc9?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80";
+  const image2 = "https://images.unsplash.com/photo-1617206685607-fc9045cb3e5b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80";
+
+    return (
+        <div className="">
+          <Nav />
+          <Card imagen={image1} />
+          <Card imagen={image2} />
+        </div>
+    );
 }
 
 export default App;
