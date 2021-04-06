c3fdece5048aa1d829294e422cbedc5c65bb847d
JJFAO <jjfalonso@gmail.com>
2021-04-06 04:48:17 -0300

    React: array de jsx y uso de states.

--- modulo3/primer-proyecto-react/src/App.js
+++ modulo3/primer-proyecto-react/src/App.js
@@ -3,15 +3,41 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from './components/Nav';
 import Card from './components/Card';
 
+const articulos = [
+    {
+        image: 'https://fotos.perfil.com/2019/01/01/trim/1280/720/10-toyota-corolla.jpg',
+        titulo: 'Corola',
+        descripcion: 'Modelo 2019',
+    },
+    {
+        image: 'https://fotos.perfil.com/2019/01/01/trim/1280/720/10-toyota-corolla.jpg',
+        titulo: 'Hilux',
+        descripcion: 'Modelo 2020',
+    },
+    {
+        image: 'https://fotos.perfil.com/2019/01/01/trim/1280/720/10-toyota-corolla.jpg',
+        titulo: 'Etios',
+        descripcion: 'Modelo 2021',
+    },
+];
+
 function App() {
-  const image1 = "https://images.unsplash.com/photo-1593642634402-b0eb5e2eebc9?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80";
-  const image2 = "https://images.unsplash.com/photo-1617206685607-fc9045cb3e5b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80";
+    const image1 =
+        'https://images.unsplash.com/photo-1593642634402-b0eb5e2eebc9?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80';
+    const image2 =
+        'https://images.unsplash.com/photo-1617206685607-fc9045cb3e5b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80';
 
     return (
         <div className="">
-          <Nav />
-          <Card imagen={image1} />
-          <Card imagen={image2} />
+            <Nav />
+            {/* <Card imagen={image1} /> */}
+            {/* <Card imagen={image2} /> */}
+
+            {articulos.map((articulo) => (
+                <Card imagen={articulo.image} titulo={articulo.titulo} descripcion={articulo.descripcion} />
+            ))}
+        
+        
         </div>
     );
 }
--- modulo3/primer-proyecto-react/src/components/Card.jsx
+++ modulo3/primer-proyecto-react/src/components/Card.jsx
@@ -1,24 +1,16 @@
 export default function Card(props) {
-    console.log('Card - props', props);
-    
     return (
-        <div className="card" style={{ width: '18rem' }}>
-            <img
-                src={props.imagen}
-                className="card-img-top"
-                alt="..."
-            />
-            <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">
-                    Some quick example text to build on the card title and make up the bulk of the card's
-                    content.
-                </p>
-                <a href="#" className="btn btn-primary">
-                    Go somewhere
-                </a>
+        <div className="container py-5">
+            <div className="card" style={{ width: '18rem' }}>
+                <img src={props.imagen} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{props.titulo}</h5>
+                    <p className="card-text">{props.descripcion}</p>
+                    <a href="#" className="btn btn-primary">
+                        Go somewhere
+                    </a>
+                </div>
             </div>
         </div>
     );
 }
-
