5063615e44e14fc10257ed4e0fec139392c90195
JJFAO <jjfalonso@gmail.com>
2021-04-26 20:10:18 -0300

    React: creacion de proyecto para react router dom

---
+++ modulo3/react-router/src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from './components/Card';
+import NavReactB from './components/NavReactB';
+
+
+export default function App() {
+
+
+    return (
+        <div className="">
+            <NavReactB />
+        </div>
+    );
+}
